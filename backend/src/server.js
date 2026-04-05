8fe05a7e3dd2f37e6724c8920619308707afaafe
sarim-alis <sarimslayerali786@gmail.com>
2026-04-05 17:41:55 +0500

    feats

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
+// Imports.
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
+// App.
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).json({ message: "Server health is good. 🔋" });
-})
+// Routes.
+app.get("/", (req, res) => { res.status(200).json({ message: "Server health is good. 🔋" });})
 
+// Server.
 app.listen(3000, () => console.log("Server is running on port 3000 🍭🌟🚀"))
