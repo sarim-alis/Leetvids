c7ad2bb6c156a7bbccc43a72f9309c85827651c8
sarim-alis <sarimslayerali786@gmail.com>
2026-04-05 17:38:29 +0500

    feats

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: "Server health is good. 🔋" });
 })
 
-app.listen(3000, () => console.log("Server is running on port 3000"))
+app.listen(3000, () => console.log("Server is running on port 3000 🍭🌟🚀"))
