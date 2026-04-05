977722efb42b6175b33c6287bca645d620353e6b
sarim-alis <sarimslayerali786@gmail.com>
2026-04-05 18:46:57 +0500

    feats

--- backend/src/lib/lib.js
+++ backend/src/lib/lib.js
@@ -5,7 +5,7 @@ import { ENV } from "./env.js";
 export const connectDb = async () => {
     try {
         await mongoose.connect(ENV.DB_URL);
-        console.log("Connected to MongoDB 🗄️");
+        console.log("MongoDB Connected 🗄️");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
         process.exit(1);
