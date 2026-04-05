0de2d236f355bb7d93fbd742c23b450e5a523578
sarim-alis <sarimslayerali786@gmail.com>
2026-04-05 20:43:49 +0500

    feat: inngest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import { SignInButton } from "@clerk/react";
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/clerk-react";
+
 
 function App() {
   return (
     <>
         <h1>Welcome to app ⭐</h1>
-        <SignInButton />
+
+        <SignedOut>
+         <SignInButton mode="modal" >
+          <button className="">Sign up</button>
+         </SignInButton>
+        </SignedOut>
+
+        <SignedIn>
+          <SignOutButton />
+        </SignedIn>
+
+        <UserButton />
     </>
-  )
+  );
 }
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ClerkProvider } from '@clerk/react'
+import { ClerkProvider } from '@clerk/clerk-react'
 const PublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PublishableKey) {
   throw new Error("Missing clerk publishable key.");
