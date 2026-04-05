1774ae855102effae45c88c5aab67ff0d40d1fb7
sarim-alis <sarimslayerali786@gmail.com>
2026-04-05 18:51:01 +0500

    feats

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react'
+const PublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if (!PublishableKey) {
+  throw new Error("Missing clerk publishable key.");
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   <ClerkProvider publishableKey={PublishableKey}>
     <App />
+   </ClerkProvider>
   </StrictMode>,
 )
