d7d1637e7e8a49ce8ff3cf9504f003ddf54d3235
sarim-alis <sarimslayerali786@gmail.com>
2026-04-25 20:33:37 -0700

    feat: sync inngest

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -5,8 +5,7 @@ import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 export const inngest = new Inngest({ id: "leet-vid" });
 
 const syncUser = inngest.createFunction(
-  { id: "sync-user" },
-  { event: "clerk/user.created" },
+  { id: "sync-user", triggers: [{ event: "clerk/user.created" }] },
   async ({ event }) => {
     await connectDb();
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
@@ -18,8 +17,7 @@ const syncUser = inngest.createFunction(
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  { id: "delete-user-from-db" },
-  { event: "clerk/user.deleted" },
+  { id: "delete-user-from-db", triggers: [{ event: "clerk/user.deleted" }] },
   async ({ event }) => {
     await connectDb();
 
