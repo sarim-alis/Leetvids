bf62bf59eb9ab415fb4ae77ea4861715f3207005
sarim-alis <sarimslayerali786@gmail.com>
2026-05-01 23:23:07 -0700

    feat: daisy ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,12 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@c
 function App() {
   return (
     <>
-        <h1>Welcome to app ⭐</h1>
+        <h1 className="text-orange-300 font-bold">Welcome to app ⭐</h1>
+        <button className="btn btn-primary">Daisuy ui</button>
 
         <SignedOut>
-         <SignInButton mode="modal" >
-          <button>Sign up</button>
+         <SignInButton mode="modal">
+          <button>Login</button>
          </SignInButton>
         </SignedOut>
 
