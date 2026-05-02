0a380a1b433f82b6ea54574db8764b8ce6e3a47e
sarim-alis <sarimslayerali786@gmail.com>
2026-05-01 23:39:03 -0700

    feat: react routet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from "react-router"
 const PublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PublishableKey) {
   throw new Error("Missing clerk publishable key.");
@@ -10,8 +11,10 @@ if (!PublishableKey) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <ClerkProvider publishableKey={PublishableKey}>
-    <App />
-   </ClerkProvider>
+   <BrowserRouter>
+    <ClerkProvider publishableKey={PublishableKey}>
+     <App />
+    </ClerkProvider>
+   </BrowserRouter>
   </StrictMode>,
 )
