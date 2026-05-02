40743c07f0ebcad487db9e36c95923cd4c8d3b3d
sarim-alis <sarimslayerali786@gmail.com>
2026-05-01 21:34:38 -0700

    feat: session routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,8 @@ import { connectDb } from "./lib/lib.js";
 import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
 import clerkRoutes from "./routes/clerk.js";
-import chatRoutes from "./routes/chatRoutes.js"
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoutes.js";
 dotenv.config();
 
 // App.
@@ -22,6 +23,7 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => { res.status(200).json({ message: "Server health is good. 🔋" });})
 app.use("/api/clerk", clerkRoutes);
 app.use("/api/chats", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 // Server.
 app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT} 🍭🌟🚀`)); connectDb()
