8608b69fa9be336106c4c2d1b7335f20dce65408
sarim-alis <sarimslayerali786@gmail.com>
2026-05-02 17:53:20 -0700

    feats

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,17 +4,23 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { BrowserRouter } from "react-router"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const PublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PublishableKey) {
   throw new Error("Missing clerk publishable key.");
 }
 
+// Create a client
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <BrowserRouter>
-    <ClerkProvider publishableKey={PublishableKey}>
-     <App />
-    </ClerkProvider>
-   </BrowserRouter>
+   <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PublishableKey}>
+       <App />
+      </ClerkProvider>
+    </BrowserRouter>
+   </QueryClientProvider>
   </StrictMode>,
 )
